3b8338f65dbab71741e365797fc79107dc797d2b
Roberto Valenti <robbyvalenti@gmail.com>
2021-03-19 16:14:58 +0100

    Added some text interface

--- disconference.js
+++ disconference.js
@@ -26,25 +26,26 @@ const wss = new Server({ server });
 
 //console.log("Server listening on port " + +process.env.PORT || 1337)
 wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+  //console.log('Client connected');
+  ws.send(JSON.stringify(Object.assign({}, rooms)))
+  //ws.on('close', () => console.log('Client disconnected'));
 });
 
 //receiving requests
-wss.on('request', function (request) {
-  i = i++;
-  connections[i] = request.accept(null, request.origin);
-  (connections[i]).on('message', function (message) {
-    // Metodo eseguito alla ricezione di un messaggio
-    if (message.type === 'utf8') {
-      // Se il messaggio è una stringa, possiamo leggerlo come segue:
-      console.log('Received mesage: ' + message.utf8Data);
-    }
-  });
-  connections[i].on('close', function (connection) {
-    // Metodo eseguito alla chiusura della connessione
-  });
-});
+// wss.on('request', function (request) {
+//   i = i++;
+//   connections[i] = request.accept(null, request.origin);
+//   (connections[i]).on('message', function (message) {
+//     // Metodo eseguito alla ricezione di un messaggio
+//     if (message.type === 'utf8') {
+//       // Se il messaggio è una stringa, possiamo leggerlo come segue:
+//       console.log('Received mesage: ' + message.utf8Data);
+//     }
+//   });
+//   connections[i].on('close', function (connection) {
+//     // Metodo eseguito alla chiusura della connessione
+//   });
+// });
 
 /*
  DISCORD.JS VERSION 12 CODE
@@ -78,7 +79,7 @@ function filterByParticipant(item, name) {
 }
 
 var rooms = []; // empty Array, which you can push() values into
-
+var programme = [];
 // client.channels.get(rooms[0]).on('speaking', (user, speaking) => {
 //   console.log(user)
 // })
@@ -170,12 +171,11 @@ client.on('voiceStateUpdate', (oldRoom, newRoom) => {
       }
     }
   }
-
-  //console.log(message)
   console.log('"'+ Date.now() + '": {')
   console.log('"event": {' + message + '},')
   console.log('"status": '+JSON.stringify(Object.assign({}, rooms)))
   console.log('},')
+
   // communicate the room update to every (active) connection
   wss.clients.forEach((client) => {
     client.send(JSON.stringify(Object.assign({}, rooms)))
@@ -183,3 +183,154 @@ client.on('voiceStateUpdate', (oldRoom, newRoom) => {
 });
 
 client.login(config.token);
+
+//===== message based interface ============
+
+var stats = {};
+
+function arrayRemove(arr, value) {
+	return arr.filter(function (ele) {
+		return ele != value;
+	});
+};
+
+client.on('message', msg => {
+	if (msg.content.charAt(0) === '!') {
+		msg.react('👀')
+			.catch(log => {
+				console.log(error);
+			});
+	};
+
+	if (msg.content === '!purpose') {
+		msg.channel.send('I wish I could voice my opinions... but I guess I\'m only here to listen... \¯\\\_\(\ツ\)\_\/\¯')
+			.catch(error => {
+				console.log(error);
+			});
+	};
+
+	if (msg.content === '!ping') {
+		msg.channel.send('Pong.')
+			.catch(error => {
+				console.log(error);
+			});
+	};
+
+	if (msg.content === '!help') {
+		msg.channel.send('Debate Bot moniters a VC to measure stats about the people speaking in it. To use Debate Bot: \n    - join a VC \n    - type `!debate` in any channel \nTo check your stats type `!stats` in any channel. If you\'re not sure whether or not Debate Bot is working, you can type `!ping` and Debate Bot should promptly reply.')
+			.catch(error => {
+				console.log(error);
+			});
+	};
+
+	if (msg.content === '!schedule') {
+   // console.log(msg.content[0], msg.content[1], msg.content[3])
+    let user = msg.author
+    let time = msg.content.split(" ")[1]
+    let topic = msg.content.splice(msg.content.indexOf( msg.content.split(" ")[2] ))
+    console.log (user + " has scheduled " + topic + "at " + time)
+    msg.channel.send(user + " has scheduled " + topic + "at " + time)
+    .catch(error => {
+      console.log(error);
+    });
+	}
+
+	if (msg.content === '!stats') {
+		// check if user in in table of recorded users:
+		user = msg.author
+		if (!(user in stats)) {
+			stats[user] = {
+				'interruptions': 0,
+				'timeSpeaking': 0,
+			};
+			msg.channel.send('it seems like you haven\'t participated in a VC debate with me yet!')
+				.catch(error => {
+					console.log(error);
+				});
+		} else {
+		interruptions = stats[user]['interruptions'];
+		timeSpeaking = stats[user]['timeSpeaking'];
+		msg.channel.send(`You've spoken in VC a total of ${timeSpeaking} minutes \nYou've interrupted others a total of ${interruptions} times.`)
+			.catch(error => {
+				console.log(error);
+			});
+		};
+	};
+
+	if (msg.content === '!debate') {
+
+		if (!msg.guild) {
+			msg.channel.send('You\'re currently not in a guild!');
+			return;
+		};
+
+		msg.channel.send('Please wait a moment while I connect to the VC.')
+			.catch(error => {
+				console.log(error);
+			});
+
+		const channel = msg.member.voiceChannel
+		if (channel === undefined) {
+			msg.channel.send('It seems like you\'re not in a VC.');
+			return
+		};
+
+		channel.join()
+			.then(connection => {
+				msg.channel.send('Success! I\'m now connected and ready to listen.');
+				var speakers = [];
+				connection.on('speaking', (user, speaking) => {
+					// when a user starts speaking:
+					if (speaking) {
+						// add them to the list of speakers
+						// start a timer recording how long they're speaking
+						speakers.push({ user: new Date().getTime() });
+						// check for interruptions
+						if (user in stats) {
+							if (speakers.length > 1) {
+								stats[user][interruptions] = stats[user]['interruptions'] + 1;
+							};
+						} else {
+							stats[user] = {
+								'interruptions': 0,
+								'timeSpeaking': 0,
+							};
+						};
+						// when a user stops speaking:
+					} else {
+						// stop the timer and add it to their total time
+						var talkTime = (new Date().getTime()) - (stats[user]['timeSpeaking']);
+						var talkTime = talkTime / 1000;
+						var talkTime = Math.abs(talkTime);
+
+						if (user in stats) {
+							stats[user]['timeSpeaking'] = stats[user]['timeSpeaking'] + talkTime;
+						} else {
+							stats[user] = {
+								'interruptions': 0,
+								'timeSpeaking': talkTime,
+							};
+						};
+						// remove them from the list of speakers
+						if (speakers.includes(user)) {
+							arrayRemove(speakers, user)
+						};
+					};
+				});
+				connection.on('disconnect', () => {
+					msg.channel.send('I was disconnected ⁉️.')
+						.catch(error => {
+							console.log(error);
+						});
+				})
+				// when a user leaves the VC:
+				//     remove them from the list of speakers
+				//     stop the timer and add it to their total time 
+			})
+			.catch(error => {
+				console.log(error);
+				msg.channel.send('I couldn\'t connect to the VC 🤔.');
+				msg.channel.send('(The VPS this is running on probably doesn\'t have the FFMPEG library installed, TBH.)')
+			});
+	};
+});
